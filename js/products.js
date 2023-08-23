d7496f7484e7a1b2e3b1a4af709098a9be0f1970
jose salim <josesalim22@yahoo.com>
2023-08-23 15:37:03 -0300

    primer commit

---
+++ js/products.js
@@ -0,0 +1,34 @@
+const productos = [
+  {
+    id: 1,
+    nombre: "Anillo de Oro",
+    precio: 52500,
+    img:
+      "./assets/img/anillo-oro.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 2,
+    nombre: "Aros de Oro",
+    precio: 21200,
+    img:
+      "./assets/img/aros-oro.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 3,
+    nombre: "Dije de plata",
+    precio: 7500,
+    img:
+      "./assets/img/dije-plata.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 4,
+    nombre: "Pulsera de plata",
+    precio: 15300,
+    img:
+      "./assets/img/pulsera-plata.jpg",
+    cantidad: 1,
+  },
+];
